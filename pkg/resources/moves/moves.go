11bb20f65d03b287540ae4a301856f0407acbbbb
Josh Guarino <jguarino722@gmail.com>
2025-03-03 13:57:05 -0500

    add moves resource group

--- pkg/resources/moves/moves.go
+++ pkg/resources/moves/moves.go
@@ -1,13 +1,159 @@
 package moves
 
+import (
+	"github.com/JoshGuarino/PokeGo/internal/constants"
+	"github.com/JoshGuarino/PokeGo/internal/request"
+	"github.com/JoshGuarino/PokeGo/pkg/models"
+)
+
 // Moves group interface
 type IMoves interface {
+	GetMove(nameOrId string) (*models.Move, error)
+	GetMoveList(options models.PaginationOptions) (*models.ResourceList, error)
+	GetMoveAilment(nameOrId string) (*models.MoveAilment, error)
+	GetMoveAilmentList(options models.PaginationOptions) (*models.ResourceList, error)
+	GetMoveBattleStyle(nameOrId string) (*models.MoveBattleStyle, error)
+	GetMoveBattleStyleList(options models.PaginationOptions) (*models.ResourceList, error)
+	GetMoveCategory(nameOrId string) (*models.MoveCategory, error)
+	GetMoveCategoryList(options models.PaginationOptions) (*models.ResourceList, error)
+	GetMoveDamageClass(nameOrId string) (*models.MoveDamageClass, error)
+	GetMoveDamageClassList(options models.PaginationOptions) (*models.ResourceList, error)
+	GetMoveLearnMethod(nameOrId string) (*models.MoveLearnMethod, error)
+	GetMoveLearnMethodList(options models.PaginationOptions) (*models.ResourceList, error)
+	GetMoveTarget(nameOrId string) (*models.MoveTarget, error)
+	GetMoveTargetList(options models.PaginationOptions) (*models.ResourceList, error)
 }
 
 // Moves group struct
 type Moves struct{}
 
-// Return an instance of Pokmon resource group struct
+// Return an instance of Moves resource group struct
 func NewMovesGroup() Moves {
 	return Moves{}
+}
+
+// Return a single Move resource by name or ID
+func (m Moves) GetMove(nameOrId string) (*models.Move, error) {
+	move, err := request.GetSpecificResource[models.Move](constants.MoveEndpoint + nameOrId)
+	if err != nil {
+		return nil, err
+	}
+	return move, nil
+}
+
+// Return a list of Move resource
+func (m Moves) GetMoveList(options models.PaginationOptions) (*models.ResourceList, error) {
+	moveList, err := request.GetResourceList(constants.MoveEndpoint, options)
+	if err != nil {
+		return nil, err
+	}
+	return moveList, nil
+}
+
+// Return a single MoveAilment resource by name or ID
+func (m Moves) GetMoveAilment(nameOrId string) (*models.MoveAilment, error) {
+	moveAilment, err := request.GetSpecificResource[models.MoveAilment](constants.MoveAilmentEndpoint + nameOrId)
+	if err != nil {
+		return nil, err
+	}
+	return moveAilment, nil
+}
+
+// Return a list of MoveAilment resource
+func (m Moves) GetMoveAilmentList(options models.PaginationOptions) (*models.ResourceList, error) {
+	moveAilmentList, err := request.GetResourceList(constants.MoveAilmentEndpoint, options)
+	if err != nil {
+		return nil, err
+	}
+	return moveAilmentList, nil
+}
+
+// Return a single MoveBattleStyle resource by name or ID
+func (m Moves) GetMoveBattleStyle(nameOrId string) (*models.MoveBattleStyle, error) {
+	moveBattleStyle, err := request.GetSpecificResource[models.MoveBattleStyle](constants.MoveBattleStyleEndpoint + nameOrId)
+	if err != nil {
+		return nil, err
+	}
+	return moveBattleStyle, nil
+}
+
+// Return a list of MoveBattleStyle resource
+func (m Moves) GetMoveBattleStyleList(options models.PaginationOptions) (*models.ResourceList, error) {
+	moveBattleStyleList, err := request.GetResourceList(constants.MoveBattleStyleEndpoint, options)
+	if err != nil {
+		return nil, err
+	}
+	return moveBattleStyleList, nil
+}
+
+// Return a single MoveCategory resource by name or ID
+func (m Moves) GetMoveCategory(nameOrId string) (*models.MoveCategory, error) {
+	moveCategory, err := request.GetSpecificResource[models.MoveCategory](constants.MoveCategoryEndpoint + nameOrId)
+	if err != nil {
+		return nil, err
+	}
+	return moveCategory, nil
+}
+
+// Return a list of MoveCategory resource
+func (m Moves) GetMoveCategoryList(options models.PaginationOptions) (*models.ResourceList, error) {
+	moveCategoryList, err := request.GetResourceList(constants.MoveCategoryEndpoint, options)
+	if err != nil {
+		return nil, err
+	}
+	return moveCategoryList, nil
+}
+
+// Return a single MoveDamageClass resource by name or ID
+func (m Moves) GetMoveDamageClass(nameOrId string) (*models.MoveDamageClass, error) {
+	moveDamageClass, err := request.GetSpecificResource[models.MoveDamageClass](constants.MoveDamageClassEndpoint + nameOrId)
+	if err != nil {
+		return nil, err
+	}
+	return moveDamageClass, nil
+}
+
+// Return a list of MoveDamageClass resource
+func (m Moves) GetMoveDamageClassList(options models.PaginationOptions) (*models.ResourceList, error) {
+	moveDamageClassList, err := request.GetResourceList(constants.MoveDamageClassEndpoint, options)
+	if err != nil {
+		return nil, err
+	}
+	return moveDamageClassList, nil
+}
+
+// Return a single MoveLearnMethod resource by name or ID
+func (m Moves) GetMoveLearnMethod(nameOrId string) (*models.MoveLearnMethod, error) {
+	moveLearnMethod, err := request.GetSpecificResource[models.MoveLearnMethod](constants.MoveLearnMethodEndpoint + nameOrId)
+	if err != nil {
+		return nil, err
+	}
+	return moveLearnMethod, nil
+}
+
+// Return a list of MoveLearnMethod resource
+func (m Moves) GetMoveLearnMethodList(options models.PaginationOptions) (*models.ResourceList, error) {
+	moveLearnMethodList, err := request.GetResourceList(constants.MoveLearnMethodEndpoint, options)
+	if err != nil {
+		return nil, err
+	}
+	return moveLearnMethodList, nil
+}
+
+// Return a single MoveTarget resource by name or ID
+func (m Moves) GetMoveTarget(nameOrId string) (*models.MoveTarget, error) {
+	moveTarget, err := request.GetSpecificResource[models.MoveTarget](constants.MoveTargetEndpoint + nameOrId)
+	if err != nil {
+		return nil, err
+	}
+	return moveTarget, nil
+}
+
+// Return a list of MoveTarget resource
+func (m Moves) GetMoveTargetList(options models.PaginationOptions) (*models.ResourceList, error) {
+	moveTargetList, err := request.GetResourceList(constants.MoveTargetEndpoint, options)
+	if err != nil {
+		return nil, err
+	}
+	return moveTargetList, nil
 }
--- pkg/resources/moves/moves_test.go
+++ pkg/resources/moves/moves_test.go
@@ -1,1 +1,129 @@
 package moves
+
+import (
+	"testing"
+
+	"github.com/JoshGuarino/PokeGo/pkg/models"
+	"github.com/stretchr/testify/assert"
+)
+
+var moves IMoves = NewMovesGroup()
+
+func TestGetMove(t *testing.T) {
+	rById, _ := moves.GetMove("1")
+	rByName, _ := moves.GetMove("pound")
+	_, err := moves.GetMove("test")
+	assert.Equal(t, 1, rById.ID, "Unexpected ID for Move resource")
+	assert.Equal(t, "pound", rByName.Name, "Unexpected Name for Move resource")
+	assert.Error(t, err, "Expected an error to be thrown.")
+}
+
+func TestGetMoveList(t *testing.T) {
+	rList, _ := moves.GetMoveList(models.PaginationOptions{})
+	rPage, _ := moves.GetMoveList(models.PaginationOptions{Limit: 1, Offest: 1})
+	assert.Equal(t, "pound", rList.Results[0].Name, "Unexpected Name for Move resource")
+	assert.Equal(t, "karate-chop", rPage.Results[0].Name, "Unexpected Name for Move resource")
+	assert.Equal(t, 1, len(rPage.Results), "Unexpected number of results returned")
+}
+
+func TestGetMoveAilment(t *testing.T) {
+	rById, _ := moves.GetMoveAilment("1")
+	rByName, _ := moves.GetMoveAilment("unknown")
+	_, err := moves.GetMoveAilment("test")
+	assert.Equal(t, 1, rById.ID, "Unexpected ID for MoveAilment resource")
+	assert.Equal(t, "unknown", rByName.Name, "Unexpected Name for MoveAilment resource")
+	assert.Error(t, err, "Expected an error to be thrown.")
+}
+
+func TestGetMoveAilmentList(t *testing.T) {
+	rList, _ := moves.GetMoveAilmentList(models.PaginationOptions{})
+	rPage, _ := moves.GetMoveAilmentList(models.PaginationOptions{Limit: 1, Offest: 1})
+	assert.Equal(t, "unknown", rList.Results[0].Name, "Unexpected Name for MoveAilment resource")
+	assert.Equal(t, "none", rPage.Results[0].Name, "Unexpected Name for MoveAilment resource")
+	assert.Equal(t, 1, len(rPage.Results), "Unexpected number of results returned")
+}
+
+func TestGetMoveBattleStyle(t *testing.T) {
+	rById, _ := moves.GetMoveBattleStyle("1")
+	rByName, _ := moves.GetMoveBattleStyle("attack")
+	_, err := moves.GetMoveBattleStyle("test")
+	assert.Equal(t, 1, rById.ID, "Unexpected ID for MoveBattleStyle resource")
+	assert.Equal(t, "attack", rByName.Name, "Unexpected Name for MoveBattleStyle resource")
+	assert.Error(t, err, "Expected an error to be thrown.")
+}
+
+func TestGetMoveBattleStyleList(t *testing.T) {
+	rList, _ := moves.GetMoveBattleStyleList(models.PaginationOptions{})
+	rPage, _ := moves.GetMoveBattleStyleList(models.PaginationOptions{Limit: 1, Offest: 1})
+	assert.Equal(t, "attack", rList.Results[0].Name, "Unexpected Name for MoveBattleStyle resource")
+	assert.Equal(t, "defense", rPage.Results[0].Name, "Unexpected Name for MoveBattleStyle resource")
+	assert.Equal(t, 1, len(rPage.Results), "Unexpected number of results returned")
+}
+
+func TestGetMoveCategory(t *testing.T) {
+	rById, _ := moves.GetMoveCategory("1")
+	rByName, _ := moves.GetMoveCategory("damage")
+	_, err := moves.GetMoveCategory("test")
+	assert.Equal(t, 1, rById.ID, "Unexpected ID for MoveCategory resource")
+	assert.Equal(t, "damage", rByName.Name, "Unexpected Name for MoveCategory resource")
+	assert.Error(t, err, "Expected an error to be thrown.")
+}
+
+func TestGetMoveCategoryList(t *testing.T) {
+	rList, _ := moves.GetMoveCategoryList(models.PaginationOptions{})
+	rPage, _ := moves.GetMoveCategoryList(models.PaginationOptions{Limit: 1, Offest: 1})
+	assert.Equal(t, "damage", rList.Results[0].Name, "Unexpected Name for MoveCategory resource")
+	assert.Equal(t, "ailment", rPage.Results[0].Name, "Unexpected Name for MoveCategory resource")
+	assert.Equal(t, 1, len(rPage.Results), "Unexpected number of results returned")
+}
+
+func TestGetMoveDamageClass(t *testing.T) {
+	rById, _ := moves.GetMoveDamageClass("1")
+	rByName, _ := moves.GetMoveDamageClass("status")
+	_, err := moves.GetMoveDamageClass("test")
+	assert.Equal(t, 1, rById.ID, "Unexpected ID for MoveDamageClass resource")
+	assert.Equal(t, "status", rByName.Name, "Unexpected Name for MoveDamageClass resource")
+	assert.Error(t, err, "Expected an error to be thrown.")
+}
+
+func TestGetMoveDamageClassList(t *testing.T) {
+	rList, _ := moves.GetMoveDamageClassList(models.PaginationOptions{})
+	rPage, _ := moves.GetMoveDamageClassList(models.PaginationOptions{Limit: 1, Offest: 1})
+	assert.Equal(t, "status", rList.Results[0].Name, "Unexpected Name for MoveDamageClass resource")
+	assert.Equal(t, "physical", rPage.Results[0].Name, "Unexpected Name for MoveDamageClass resource")
+	assert.Equal(t, 1, len(rPage.Results), "Unexpected number of results returned")
+}
+
+func TestGetMoveLearnMethod(t *testing.T) {
+	rById, _ := moves.GetMoveLearnMethod("1")
+	rByName, _ := moves.GetMoveLearnMethod("level-up")
+	_, err := moves.GetMoveLearnMethod("test")
+	assert.Equal(t, 1, rById.ID, "Unexpected ID for MoveLearnMethod resource")
+	assert.Equal(t, "level-up", rByName.Name, "Unexpected Name for MoveLearnMethod resource")
+	assert.Error(t, err, "Expected an error to be thrown.")
+}
+
+func TestGetMoveLearnMethodList(t *testing.T) {
+	rList, _ := moves.GetMoveLearnMethodList(models.PaginationOptions{})
+	rPage, _ := moves.GetMoveLearnMethodList(models.PaginationOptions{Limit: 1, Offest: 1})
+	assert.Equal(t, "level-up", rList.Results[0].Name, "Unexpected Name for MoveLearnMethod resource")
+	assert.Equal(t, "egg", rPage.Results[0].Name, "Unexpected Name for MoveLearnMethod resource")
+	assert.Equal(t, 1, len(rPage.Results), "Unexpected number of results returned")
+}
+
+func TestGetMoveTarget(t *testing.T) {
+	rById, _ := moves.GetMoveTarget("1")
+	rByName, _ := moves.GetMoveTarget("specific-move")
+	_, err := moves.GetMoveTarget("test")
+	assert.Equal(t, 1, rById.ID, "Unexpected ID for MoveTarget resource")
+	assert.Equal(t, "specific-move", rByName.Name, "Unexpected Name for MoveTarget resource")
+	assert.Error(t, err, "Expected an error to be thrown.")
+}
+
+func TestGetMoveTargetList(t *testing.T) {
+	rList, _ := moves.GetMoveTargetList(models.PaginationOptions{})
+	rPage, _ := moves.GetMoveTargetList(models.PaginationOptions{Limit: 1, Offest: 1})
+	assert.Equal(t, "specific-move", rList.Results[0].Name, "Unexpected Name for MoveTarget resource")
+	assert.Equal(t, "selected-pokemon-me-first", rPage.Results[0].Name, "Unexpected Name for MoveTarget resource")
+	assert.Equal(t, 1, len(rPage.Results), "Unexpected number of results returned")
+}
