4495a954b0cb73d79666e49228c57120fe7984af
Josh Guarino <jguarino722@gmail.com>
2025-03-06 10:51:27 -0500

    add cache files

--- internal/cache/cache.go
+++ internal/cache/cache.go
@@ -1,1 +1,66 @@
 package cache
+
+import (
+	"sync"
+	"time"
+)
+
+// Cache interface
+type ICache interface {
+	Get(key string) (any, bool)
+	Set(key string, value any, expiration time.Duration)
+	Clear()
+}
+
+// Cache struct
+type Cache struct {
+	data map[string]Value
+	lock sync.Mutex
+}
+
+// Value struct
+type Value struct {
+	value      any
+	expiration time.Time
+}
+
+// Return an instance of Cache struct
+func NewCache() *Cache {
+	return &Cache{
+		data: make(map[string]Value),
+	}
+}
+
+// Set a key-value pair in the cache
+func (c *Cache) Set(key string, value any, expiration time.Duration) {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+
+	expirationTime := time.Now().Add(expiration)
+	c.data[key] = Value{
+		value:      value,
+		expiration: expirationTime,
+	}
+}
+
+// Get a value from the cache by key
+func (c *Cache) Get(key string) (any, bool) {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+
+	value, ok := c.data[key]
+	if !ok || time.Now().After(value.expiration) {
+		delete(c.data, key)
+		return nil, false
+	}
+
+	return value.value, true
+}
+
+// Clear the cache
+func (c *Cache) Clear() {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+
+	c.data = make(map[string]Value)
+}
--- internal/cache/cache_test.go
+++ internal/cache/cache_test.go
@@ -1,1 +1,34 @@
 package cache
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var cache = NewCache()
+
+func TestNewCache(t *testing.T) {
+	assert.IsType(t, &Cache{}, cache)
+}
+
+func TestSet(t *testing.T) {
+	cache.Set("key", "value", 5*time.Second)
+	assert.Equal(t, "value", cache.data["key"].value, "Unexpected value in cache")
+}
+
+func TestGet(t *testing.T) {
+	cache.Set("key", "value", 5*time.Second)
+	value, ok := cache.Get("key")
+	assert.Equal(t, "value", value, "Unexpected value in cache")
+	assert.True(t, ok)
+}
+
+func TestCear(t *testing.T) {
+	cache.Set("key", "value", 100*time.Second)
+	cache.Clear()
+	value, ok := cache.Get("key")
+	assert.Equal(t, nil, value, "Expected value to be nil")
+	assert.False(t, ok)
+}
